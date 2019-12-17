5abc40c9229238a107831da9d7c5db2d34d05abc
Bryan <tambryanelliott@gmail.com>
2019-12-17 10:02:03 -0500

    main file started

---
+++ GIF-4101/main.py
@@ -0,0 +1,9 @@
+import pickle
+import pandas as pd
+import os
+
+dataFile = outputFile = os.path.join("Data","data.p")
+
+with open(dataFile, "rb") as input_file:
+    data = pickle.load(input_file)
+print(data)
