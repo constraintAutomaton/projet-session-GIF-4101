d895ce33bd8f00d926e3f2efc826ee2ccc7c78af
Bryan <tambryanelliott@gmail.com>
2019-12-17 10:02:25 -0500

    main file started

--- GIF-4101/main.py
+++ GIF-4101/main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 
 dataFile = outputFile = os.path.join("Data","data.p")
-
+data = None
 with open(dataFile, "rb") as input_file:
     data = pickle.load(input_file)
 print(data)
