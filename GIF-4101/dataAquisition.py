dc26857fc3ea967b949c671167c749e637430b37
Bryan <tambryanelliott@gmail.com>
2019-12-17 09:12:16 -0500

    made pathing compatible with linux system

--- GIF-4101/dataAquisition.py
+++ GIF-4101/dataAquisition.py
@@ -1,3 +1,6 @@
+
+# coding=utf-8
+
 import numpy as np
 import pandas as pd
 import torch
@@ -54,7 +57,7 @@ Province_data_to_trend = { \
 #suivre annee par un "R" 
 #Retourne nom du district, Affiliation du candidat et son pourcentage
 def Loader_resultats(Annee):
-    dir = os.getcwd() + '\Data\Resultats_uniformes.xlsx'
+    dir = os.path.join("Data","Resultats_uniformes.xlsx")
     df = pd.read_excel(dir,sheet_name=Annee)
     df = df.drop(df.columns[[3]], axis=1)
     df["Year"] = Annee
@@ -138,7 +141,7 @@ def get_info(annees, chefs, periodes):
 
 
 #exemple
-allo = get_info(Liste_electionR, Partis, Chefs, Periodes);
+allo = get_info(Liste_electionR, Chefs, Periodes);
 print(allo)
 
 for i in range(len(allo)):
