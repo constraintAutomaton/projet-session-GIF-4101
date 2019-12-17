2e9dc77e28deb33e2365bfde129500d9fefc0188
febou074 <felix.bouchard.5@ulaval.ca>
2019-12-16 19:10:13 -0500

    Formatage des données dans une seule variable

--- GIF-4101/GIF_4101.py
+++ GIF-4101/GIF_4101.py
@@ -28,18 +28,35 @@ Chef2006 = ['Paul Martin', 'Stephen Harper', 'Jack Layton', 'Gilles Duceppe']
 Chef2008 = ['Stéphane Dion', 'Stephen Harper', 'Jack Layton', 'Gilles Duceppe']
 Chef2011 = ['Michael Ignatieff', 'Stephen Harper', 'Jack Layton', 'Gilles Duceppe']
 Chef2015 = ['Justin Trudeau', 'Stephen Harper', 'Thomas Mulcair', 'Gilles Duceppe']
-
-Parti = ['Parti libéral du Canada', 'Parti conservateur du Canada', 'Nouveau Parti démocratique', 'Bloc Québécois']
+Chefs = [Chef2004, Chef2006, Chef2008, Chef2011, Chef2015]
 
 #période de la date des élections à 1 mois avant
-Periode = ['2004-05-28 2004-06-28', '2005-12-23 2006-01-23', '2008-09-14 2008-10-14', '2011-04-11 2011-05-11', '2015-09-19 2015-10-19']
+Periodes = ['2004-05-28 2004-06-28', '2005-12-23 2006-01-23', '2008-09-14 2008-10-14', '2011-04-11 2011-05-11', '2015-09-19 2015-10-19']
+
+
+Province_data_to_trend = { \
+"Newfoundland and Labrador/Terre-Neuve-et-Labrador" : "Newfoundland and Labrador", \
+"Prince Edward Island/Île-du-Prince-Édouard" : "Prince Edward Island", \
+"Nova Scotia/Nouvelle-Écosse" : "Nova Scotia", \
+"New Brunswick/Nouveau-Brunswick" : "New Brunswick", \
+"Quebec/Québec" : "Québec", \
+"Ontario" : "Ontario", \
+"Manitoba" : "Manitoba", \
+"Saskatchewan" : "Saskatchewan", \
+"Alberta" : "Alberta", \
+"British Columbia/Colombie-Britannique" : "British Columbia", \
+"Yukon" : "Yukon Territory", \
+"Northwest Territories/Territoires du Nord-Ouest" : "Northwest Territories", \
+"Nunavut" : "Nunavut"}
+
+
 
 #suivre annee par un "R" 
 #Retourne nom du district, Affiliation du candidat et son pourcentage
 def Loader_resultats(Annee):
     dir = os.getcwd() + '\Data\Resultats_uniformes.xlsx'
     df = pd.read_excel(dir,sheet_name=Annee)
-    df = df.drop(df.columns[[4]], axis=1)
+    df = df.drop(df.columns[[3]], axis=1)
     df["Year"] = Annee
     return df
 
@@ -49,9 +66,81 @@ def Loader_resultats(Annee):
 def Loader_Google_Trend(keyword,periode):
     pytrends.build_payload(keyword, cat=0, timeframe=periode, geo='CA')
     df = pytrends.interest_by_region(resolution = 'REGION', inc_low_vol=True, inc_geo_code=False)
-    #print(envifr)
     return df
 
+def get_info(annees, chefs, periodes):
+	data = pd.DataFrame(columns=["Annee", "Province", "Circonscription", "Trend chef parti libéral", \
+	 "Trend chef parti conservateur", "Trend chef parti npd", "Trend chef parti bloc quebecois", \
+	 "Pourcentage vote parti liberal", "Pourcentage vote parti conservateur", \
+	 "Pourcentage vote parti npd", "Pourcentage vote parti bloc quebecois"])
+
+	compteur = 0
+	for i in range(len(annees)):
+		resultats = Loader_resultats(annees[i])
+		annee = annees[i]
+
+		trend_chefs_parti = Loader_Google_Trend(chefs[i], periodes[i])
+
+		vote_npd = 0
+		vote_bloc = 0
+		vote_liberal = 0
+		vote_conservateur = 0
+		circonscription_precedente = ""
+
+		allo = 0
+
+		for j in range(len(resultats)):
+			circonscription = resultats.iloc[j]["Electoral District Name/Nom de circonscription"]
+
+			if circonscription != circonscription_precedente and isinstance(circonscription, str):
+
+				if j != 0 :
+					allo += 1
+					province = resultats.iloc[j-1]["Province"]
+
+					trend_province_value = Province_data_to_trend[province]
+					trend_chef_liberal = trend_chefs_parti.loc[trend_province_value][chefs[i][0]]
+					trend_chef_conservateur = trend_chefs_parti.loc[trend_province_value][chefs[i][1]]
+					trend_chef_npd = trend_chefs_parti.loc[trend_province_value][chefs[i][2]]
+					trend_chef_bloc = trend_chefs_parti.loc[trend_province_value][chefs[i][3]]
+
+					data.loc[compteur] = [annee, province, circonscription_precedente, trend_chef_liberal, trend_chef_conservateur, trend_chef_npd, trend_chef_bloc, vote_liberal, vote_conservateur, vote_npd, vote_bloc]
+				
+					compteur += 1
+		
+			if resultats.iloc[j]["Affiliation"] == "Libéral":
+				vote_liberal = resultats.iloc[j]["Percentage of Votes Obtained /Pourcentage des votes obtenus"]
+			elif resultats.iloc[j]["Affiliation"] == "conservateur":
+				vote_conservateur = resultats.iloc[j]["Percentage of Votes Obtained /Pourcentage des votes obtenus"]
+			elif resultats.iloc[j]["Affiliation"] == "N.P.D." or resultats.iloc[j]["Affiliation"] == "NPD-Nouveau Parti démocratique":
+				vote_npd = resultats.iloc[j]["Percentage of Votes Obtained /Pourcentage des votes obtenus"]
+			elif resultats.iloc[j]["Affiliation"] == "Bloc Québécois":
+				vote_bloc = resultats.iloc[j]["Percentage of Votes Obtained /Pourcentage des votes obtenus"]
+
+			circonscription_precedente = circonscription
+
+			if j == len(resultats) - 1 and isinstance(circonscription, str):
+				allo += 1
+				province = resultats.iloc[j]["Province"]
+
+				trend_province_value = Province_data_to_trend[province]
+				trend_chef_liberal = trend_chefs_parti.loc[trend_province_value][chefs[i][0]]
+				trend_chef_conservateur = trend_chefs_parti.loc[trend_province_value][chefs[i][1]]
+				trend_chef_npd = trend_chefs_parti.loc[trend_province_value][chefs[i][2]]
+				trend_chef_bloc = trend_chefs_parti.loc[trend_province_value][chefs[i][3]]
+
+				data.loc[compteur] = [annee, province, circonscription_precedente, trend_chef_liberal, trend_chef_conservateur, trend_chef_npd, trend_chef_bloc, vote_liberal, vote_conservateur, vote_npd, vote_bloc]
+				
+				compteur += 1
+	return data
+
+
+
+
 #exemple
-allo = Loader_Google_Trend(Chef2015, Periode[0])
-print(allo.iloc[0])
+allo = get_info(Liste_electionR, Partis, Chefs, Periodes);
+print(allo)
+
+for i in range(len(allo)):
+	if allo.iloc[i]["Annee"] == Liste_electionR[4]:
+		print(allo.iloc[i]["Trend chef parti libéral"])
