02d9909a591d8c502a2a98fc1327b38c3f5fd511
AlexandreLortie <46944067+AlexandreLortie@users.noreply.github.com>
2019-12-17 20:05:31 -0500

    gtrend included

--- GIF-4101/main.py
+++ GIF-4101/main.py
@@ -1,9 +1,11 @@
 import pickle
 import pandas as pd
 import os
-
+from Visualisation_data import *
 dataFile = outputFile = os.path.join("Data","data.p")
 data = None
 with open(dataFile, "rb") as input_file:
     data = pickle.load(input_file)
-X,y = (data,)
+
+visualisation()
+#X,y = (data,)
