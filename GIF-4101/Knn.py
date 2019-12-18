94afddd0ffd0e4aa1a77f4375f4c1471206b5a7d
CrispyRichy <41133262+CrispyRichy@users.noreply.github.com>
2019-12-17 22:04:00 -0500

    Create Knn.py

---
+++ GIF-4101/Knn.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import os
+from pytrends.request import TrendReq
+import pickle
+import numpy as np
+import matplotlib
+matplotlib.rcParams['figure.figsize'] = (5.0, 5.0)
+from matplotlib import pyplot
+from sklearn.model_selection import KFold
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import preprocessing
+
+
+
+
+
+dataFile = outputFile = os.path.join("Data","data.p")
+data = None
+with open(dataFile, "rb") as input_file:
+    data = pickle.load(input_file)
+data = data.dropna()
+
+for i in range(len(data)):
+    maxi = max(data.iloc[i,7],data.iloc[i,8],data.iloc[i,9],data.iloc[i,10])
+    if maxi == data.iloc[i,7]:
+        col.append("Black")
+    elif maxi == data.iloc[i,8]:
+        col.append("Red")
+    elif maxi == data.iloc[i,9]:
+        col.append("Blue")
+    elif maxi == data.iloc[i,10]:
+        col.append("Green")
+
+  
+X = data.iloc[:,[3,4,5,6,11,12,13,14]]  
+
+X = preprocessing.scale(X)
+
+
+y = np.zeros(np.shape(X)[0])
+
+scoresUniformWeights = []
+scoresDistanceWeights = []
+for i in range(len(data)):
+    maxi = max(data.iloc[i,7],data.iloc[i,8],data.iloc[i,9],data.iloc[i,10])
+    if maxi == data.iloc[i,7]:
+        y[i] == 1
+    elif maxi == data.iloc[i,8]:
+        y[i] == 2
+    elif maxi == data.iloc[i,9]:
+        y[i] == 3
+    elif maxi == data.iloc[i,10]:
+        y[i] == 4
+
+    
+k = [10,30,50,70,90,110,130,150]
+N =np.shape(X)[0]
+for i in k:
+    Avs = 0
+    RPK = KFold(n_splits=10)
+    for train,test in RPK.split(y):
+        Data_train,target_train = X[train,:],y[train]
+        Data_test,target_test = X[test,:],y[test]
+        knn = KNeighborsClassifier(n_neighbors=i, weights="uniform")
+        knn.fit(Data_train,target_train)
+        Avs +=knn.score(Data_test,target_test)
+    scoresUniformWeights.append(Avs/N)
+
+    
+
+
+for i in k:
+    Avs=0
+    RPK = KFold(n_splits=10)
+    for train,test in RPK.split(y):
+        Data_train,target_train = X[train,:],y[train]
+        Data_test,target_test = X[test,:],y[test]
+        knn = KNeighborsClassifier(n_neighbors=i, weights="distance")
+        knn.fit(Data_train,target_train)
+        Avs +=knn.score(Data_test,target_test)
+    scoresDistanceWeights.append(Avs/N) 
+
+
+pyplot.plot(np.array(k),np.array(scoresUniformWeights),label= "Uniform")
+pyplot.plot(np.array(k),np.array(scoresDistanceWeights),label = "Distance")
+pyplot.legend()
+pyplot.show()
