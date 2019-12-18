0f9830805d07bdff25571558819b6c737042a85d
Bryan <tambryanelliott@gmail.com>
2019-12-17 20:06:40 -0500

    svm started

--- GIF-4101/main.py
+++ GIF-4101/main.py
@@ -2,8 +2,8 @@ import pickle
 import pandas as pd
 import os
 
-dataFile = outputFile = os.path.join("Data","data.p")
+dataFile  = os.path.join("Data","data.p")
 data = None
 with open(dataFile, "rb") as input_file:
     data = pickle.load(input_file)
-X,y = (data,)
+X,y = (data,1)
